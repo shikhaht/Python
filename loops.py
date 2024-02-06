1aef3f2c6c31819f697b3b37e4a65995ff12c86d
Shikha <shikhaht03@gmail.com>
2024-02-06 22:37:17 +0530

    create for loops in python

---
+++ loops.py
@@ -0,0 +1,13 @@
+ninjas = ['yno', 'luigi','shinzo']
+
+# for ninja in ninjas:
+#     print(ninja)
+
+
+# for ninja in ninjas[0:2]:
+#     print(ninja)
+
+for ninja in ninjas:
+    if ninja == 'shinzo':
+        print(ninja)
+        break
