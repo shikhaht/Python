b4ca6b4508d9fcfe69da39c2e0877c3437c6976b
Shikha <shikhaht03@gmail.com>
2024-02-06 15:20:06 +0530

    area of circle and string formatting

---
+++ area_calc.py
@@ -0,0 +1,10 @@
+#comments
+# name = input('tell me your name, shikha:')
+# print(name)
+
+
+#Calc the area of circle
+radius = input('Enter the radius of the circle: ')
+area = 3.14 * int(radius)**2
+print("The area of the circle is ", area)
+
---
+++ string_format.py
@@ -0,0 +1,11 @@
+num1 = 3.14262738793
+num2 = 10.293948
+
+#PREVIOUS
+# print('num1 is',num1,'and num2 is',num2)
+
+#FORMAT METHOD
+#print('num 1 is {0} and num 2 is {1:.3f}'.format(num1,num2))
+
+#USING F-STRINGS
+print(f'The value of num1 is {num1:.5f} and the value of num2 is {num2:.3f}.')
