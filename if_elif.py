d8dff69c3db3d470487c4a976b48829c5b436c11
Shikha <shikhaht03@gmail.com>
2024-02-06 20:49:20 +0530

    if else in python

---
+++ if_elif.py
@@ -0,0 +1,19 @@
+# age = int(input('Enter your age:'))
+
+# if age < 10:
+#     print("You are a child.")
+# elif age < 40:    
+#     print('You are Young')
+# else:
+#     print('You are old')
+
+
+meaty = input('Do you eat meat?(y/n):')
+
+if meaty == 'y':
+    print('yes you eat meat')
+
+elif meaty == 'n':
+    print('no you don\'t eat meat')
+else:
+    print('invalid input')
