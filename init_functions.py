4d48a3fb28a4bb25cca03b9895a87c4102b9285a
Shikha <shikhaht03@gmail.com>
2024-02-10 19:26:52 +0530

    init functions in python

---
+++ init_functions.py
@@ -0,0 +1,23 @@
+class Planet:
+
+    def __init__(self,name,radius,gravity,system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
+
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
+
+
+hoth =  Planet('Hoth',200000,5.5,'Hoth System')
+print(f'Name is: {hoth.name}')
+print(f'Radius is: {hoth.radius}')
+print(f'Gravity is: {hoth.gravity}')
+print(hoth.orbit())
+
+naboo = Planet('Naboo', 300000,8,'Naboo System')
+print(f'Name: {naboo.name}')
+print(f'Radius is: {naboo.radius}')
+print(f'Gravity is: {naboo.gravity}')
+print(naboo.orbit())
